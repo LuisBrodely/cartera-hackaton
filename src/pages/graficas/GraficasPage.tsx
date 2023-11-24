811888123b7dd4c8ac5f2eb18e7821f022b3c4ec
Leonardo-Toledo-V <leo.toledo.grams@gmail.com>
2023-11-24 12:05:13 -0600

    dog

--- src/pages/graficas/GraficasPage.tsx
+++ src/pages/graficas/GraficasPage.tsx
@@ -1,47 +1,90 @@
-import { Button, ButtonSocial } from '../../components';
-import apple from '../../assets/icons/apple.png'
-import google from '../../assets/icons/google.png'
-import logo from '../../assets/icons/logo.svg'
+import burguer from '../../assets/icons/category.png'
+import notis from '../../assets/icons/notification.png'
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Doughnut } from 'react-chartjs-2';
+
+ChartJS.register(ArcElement, Tooltip, Legend);
 
 export const GraficasPage = () => {
+
+    const data = {
+        datasets: [
+            {
+                label: '%',
+                data: [33, 25, 40],
+                backgroundColor: [
+                    '#DCEF64',
+                    '#F2F9C8',
+                    '#E8EBF7'
+                ],
+                borderColor: [
+                    '#DCEF64',
+                    '#F2F9C8',
+                    '#E8EBF7'
+                ],
+                borderWidth: 0.1,
+            },
+        ],
+    };
+
     return (
-        <section className="bg-[#161515] h-screen py-10 px-7">
-            <div className='flex items-center flex-col mt-20'>
-                <img src={logo} alt="logo" className='h-10 w-auto' />
-                <h1 className='font-bold text-[#f0f0f0] text-3xl mt-4'>Iniciar Sesión</h1>
-                <p className='text-[#BFBEBE]'>Ingresa tus datos para comenzar</p>
+        <>
+        <section className="bg-[#161515] h-full py-10 px-7">
+            <div className='flex items-center flex-col'>
+                <div className='flex justify-between w-full'>
+                    <img src={burguer} alt="logo" className='h-6 w-auto' />
+                    <img src={notis} alt="logo" className='h-6 w-auto' />
+                </div>
+                <div className='flex flex-col justify-center items-center'>
+                    <p className='font-medium text-[#DCEF64] text-lg mt-4'>Ahorro total</p>
+                    <p className='font-bold text-[#f0f0f0] text-[2.5rem] leading-7 mt-1'>$56,672.00</p>
+                </div>
             </div>
 
             <div className='flex items-center flex-col gap-y-2 mt-12'>
-                <div className='w-full'>
-                    <input className='w-full bg-[#161515] border-[#757575] border-[0.5px] py-4 px-4 text-sm' type="text" placeholder='Correo electrónico' />
-                </div>
-                <div className='mt-3 w-full'>
-                    <input className='w-full bg-[#161515] border-[#757575] border-[0.5px] py-4 px-4 text-sm text-[#757575] focus:border-[0.5px]' type="text" placeholder='Contraseña' />
-                </div>
-                <div className='flex justify-end w-full mt-4'>
-                    <p className='text-[#DCEF64] text-sm'>¿Olvidaste tu contraseña?</p>
-                </div>
-                <div className='w-full mt-3'>
-                    <Button className="w-full" onClick={() => { }} text='Registrate' />
-                </div>
-                <div className='w-full flex items-center justify-center mt-4'>
-                    <p className='text-[#f0f0f0] text-sm'>Crear una cuenta nueva</p>
+                <div className='relative'>
+                    <Doughnut data={data} className='cursor-pointer' />
+                    <p className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 font-bold text-xl text-[#FFFFFF]">
+                        $4,672.00 <br />
+                        <div className='flex justify-center items-center'>
+                            <p className='text-[#C8C8C8] font-medium tracking-wide'>Semanal</p>
+                        </div>
+                    </p>
                 </div>
 
-                <div className='w-full flex items-center justify-center mt-7'>
-                    <p className='text-[#DCEF64] text-sm'>También puedes entrar con</p>
-                </div>
-
-                <div className='mt-6 flex gap-5'>
-                    <ButtonSocial onClick={() => { }}>
-                        <img src={apple} alt="Apple logo" className='w-5' />
-                    </ButtonSocial>
-                    <ButtonSocial onClick={() => { }}>
-                        <img src={google} alt="Google logo" className='w-5' />
-                    </ButtonSocial>
+                <div className="flex justify-around mt-4 gap-x-6">
+                    <div className="flex items-center space-x-3 flex-1 text-sm text-gray-200 font-medium">
+                        <div className="bg-[#DCEF64] w-5 h-5 rounded-sm" />
+                        <div>
+                            <h3 className='font-bold'>Ahorro</h3>
+                            <p className='text-gray-200'>33%</p>
+                        </div>
+                    </div>
+                    <div className="flex items-center space-x-3 text-sm text-gray-200 font-medium">
+                        <div className="bg-[#F2F9C8] w-5 h-5 rounded-sm" />
+                        <div>
+                            <h3 className='font-bold'>Gastos</h3>
+                            <p className='text-gray-200'>33%</p>
+                        </div>
+                    </div>
+                    <div className="flex items-center space-x-3 text-sm text-gray-200 font-medium">
+                        <div className="bg-[#E8EBF7] w-5 h-5 rounded-sm" />
+                        <div>
+                            <h3 className='font-bold'>Ingresos</h3>
+                            <p className='text-gray-200'>33%</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
+        <section className="bg-[#ffffff] h-full py-4 px-7">
+                <div>
+                    <div className='border-b-[0.1px] border-gray-900'/>
+                    <div>
+                        <h3 className='text-black font-bold mt-4 text-sm'>Comparativa</h3>
+                    </div>
+                </div>
+        </section>
+        </>
     )
 }
