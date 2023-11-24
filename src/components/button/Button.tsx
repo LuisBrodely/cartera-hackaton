b501d7b65cd3e55edb2d4dfa38bd97b5bdb366e5
Leonardo-Toledo-V <leo.toledo.grams@gmail.com>
2023-11-24 11:10:36 -0600

    login

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -3,11 +3,14 @@ import React, { MouseEventHandler } from "react";
 interface ButtonProps {
   onClick: MouseEventHandler<HTMLButtonElement>;
   text: string;
+  className?: string;
 }
 
-export const Button: React.FC<ButtonProps> = ({ onClick, text }) => {
+export const Button: React.FC<ButtonProps> = ({ onClick, text, className }) => {
   return (
-    <button onClick={onClick} className="bg-[#DCEF64] w-[156px] h-[51px]">
+    <button 
+    onClick={onClick} 
+    className={`bg-[#DCEF64] w-[156px] h-[51px] ${className || ""}`}>
       <span className="font-medium">
 				{text}
 			</span>
--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom"
-import { RegisterPage, HomePage } from './pages';
+import { RegisterPage, HomePage, LoginPage} from './pages';
 
 const router = createBrowserRouter([
   {
@@ -14,6 +14,10 @@ const router = createBrowserRouter([
   {
     path: "/register",
     element: <RegisterPage />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
   },
 ]);
 
